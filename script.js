42c8e107833f940c681b1977d7a8f75f363f548e
JasonMcGee0715 <jdm0715@gmail.com>
2020-04-12 22:24:02 -0500

    possible loading fix

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-const header = document.getElementById("myheader");
-const sticky = header.offsetTop;
+// const header = document.getElementById("myheader");
+// const sticky = header.offsetTop;
 
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  }
+// function myFunction() {
+//     if (window.pageYOffset > sticky) {
+//       header.classList.add("sticky");
+//     } else {
+//       header.classList.remove("sticky");
+//     }
+//   }
