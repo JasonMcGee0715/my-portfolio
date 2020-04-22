c423464d8fb034c3ba7bf2ce2122951b058a55c9
JasonMcGee0715 <jdm0715@gmail.com>
2020-04-21 21:09:45 -0500

    more code

--- script.js
+++ script.js
@@ -11,17 +11,27 @@
 //     }
 //   }
 
-function preloader() {
-  let pageloader = document.querySelector('.pageloader')
-  pageloader.style.opacity = '0'
-  preloader.setAttribute('aria-busy', 'false')
-  document.getElementById('site').style.opacity = '1'
-}
-window.addEventListener('load', function(e) {
-    setTimeout(preloader(), 1500)
-    setTimeout(function() {
-        // let document.querySelector('.')
-    }, 500)
+// function preloader() {
+//   let pageloader = document.querySelector('.pageloader')
+//   pageloader.style.opacity = '0'
+//   preloader.setAttribute('aria-busy', 'false')
+//   document.getElementById('site').style.opacity = '1'
+// }
+// window.addEventListener('load', function(e) {
+//     setTimeout(preloader(), 1500)
+//     setTimeout(function() {
+//         // let document.querySelector('.')
+//     }, 500)
+// })
+
+
+
+
+const hamMenu = document.querySelector(".hamMenu")
+const ul = document.querySelector(".ul")
+
+hamMenu.addEventListener("click", function() {
+  ul.classList.toggle("focusedUserlist")
+  hamMenu.classList.toggle("menu180")
+  console.log("clicked")
 })
-
-
